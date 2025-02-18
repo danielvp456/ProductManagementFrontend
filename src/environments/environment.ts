61950f2d9500596c10b9d29b9e0ea4bbf0474c16
danielvp456 <danielvp456@hotmail.com>
2025-02-17 19:54:24 -0500

    Backend server URL modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  //apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://productmanagementbackend-638194867449.us-central1.run.app'
 }; 

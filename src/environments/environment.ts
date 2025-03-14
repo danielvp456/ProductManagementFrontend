709852c8ee887ec71c5761a94bbe009d5da5b122
danielvp456 <danielvp456@hotmail.com>
2025-03-13 22:21:25 -0500

    .env variables updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:3000'
-  apiUrl: 'https://productmanagementbackend-638194867449.us-central1.run.app'
+  //apiUrl: 'https://productmanagementbackend-638194867449.us-central1.run.app'
+  apiUrl: 'https://productmanagementbackend-1028543215102.us-central1.run.app'
 }; 
